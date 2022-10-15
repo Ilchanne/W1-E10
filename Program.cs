56190c4eed176076774d051a308c31a5eb6dad85
Ilchan <annilchen@yandex.ru>
2022-10-15 16:27:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число:");
+double a = Convert.ToDouble(Console.ReadLine());
+double c, b , d;
+b = Math.Pow(a, 2);
+c = Math.Pow(a, 3);
+d = Math.Pow(a, 4);
+Console.WriteLine($"{a} в квадрате равно {b}, в кубе {c}, в четвертой степени {d}");
